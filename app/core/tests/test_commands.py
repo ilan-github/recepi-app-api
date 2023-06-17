b8595abda80c493cf11d55c64954140dee89eb77
dvirh <dvirhaberman1@gmail.com>
2023-06-17 12:25:44 +0300

    change typos in test_commands.py

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,32 +1,33 @@
 """
-Test custon Django management commands.
+Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch('django.db.utils.ConnectionHandler.__getitem__')
 class CommandTests(SimpleTestCase):
     """Test commands."""
-    
-    def text_wait_for_db_ready(self, patched_sleep, patched_check):
-        """Test waiting for db if database ready."""
-        patched_check.return_value = True
-        
+
+    def test_wait_for_db_ready(self, patched_getitem):
+        """Test waiting for database if database ready."""
+        patched_getitem.return_value = True
+
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
-    
-    @patch('time.sleep')    
-    def test_wait_for_db_delay(self, patched_check):
-        """Test waiting for db when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] *2 + \
-            [OperationalError]*3 + [True]
-        
+
+        self.assertEqual(patched_getitem.call_count, 1)
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
+        """Test waiting for database when getting OperationalError."""
+        patched_getitem.side_effect = [Psycopg2OpError] + \
+            [OperationalError] * 5 + [True]
+
         call_command('wait_for_db')
-        self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+
+        self.assertEqual(patched_getitem.call_count, 6)

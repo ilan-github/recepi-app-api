0ba93c142a46239d704be5da754fd5c30f27c4f1
dvirh <dvirhaberman1@gmail.com>
2023-06-24 07:57:56 +0300

    typo

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -23,14 +23,14 @@ class PublicUserApiTests(TestCase):
     
     def test_create_user_success(self):
         """Test creating a user successful."""
-        paylod = {
+        payload = {
             'email': 'exst@example.com',
             'password': 'testpass123',
             'name': 'Test Name',
         }
-        res = self.client.post(CREATE_USER_URL, paylod)
+        res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)     
-        user.get_user_model().objects.get(email=payload('email'))
+        user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
         
@@ -49,13 +49,13 @@ class PublicUserApiTests(TestCase):
         """Test an error is return is password less than 5 char."""
         payload = {
             'email': 'exst@example.com',
-            'password': 'test',
-            'name': 'Test Name',  
+            'password': 'pw',
+            'name': 'Test name',  
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exist = get_user_model().objects.filter(
+        user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
-        self.assertFalse(user_exist)
+        self.assertFalse(user_exists)
